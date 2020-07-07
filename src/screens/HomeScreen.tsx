db8234b8b6e9e09950ed7e9d2fb7e23fb81b55f5
Roman Kuzmín <roman.kuzmin@ranako.cz>
2020-07-07 11:23:06 +0200

    refactor: remove unused

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Drawer, Fab, Hidden, Snackbar, Theme } from '@material-ui/core';
+import { Button, Container, Drawer, Fab, Hidden, Theme } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
